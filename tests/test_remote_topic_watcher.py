8c0f83fdc88a0cfdb8d5fe00487bbcb8972187fd
riley206 <rileymaltos@gmail.com>
2023-11-21 12:57:31 -0800

    fix file path

--- tests/test_remote_topic_watcher.py
+++ tests/test_remote_topic_watcher.py
@@ -1,47 +1,32 @@
-
 # -*- coding: utf-8 -*- {{{
-# vim: set fenc=utf-8 ft=python sw=4 ts=4 sts=4 et:
+# ===----------------------------------------------------------------------===
 #
-# Copyright 2020, Battelle Memorial Institute.
+#                 Installable Component of Eclipse VOLTTRON
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# ===----------------------------------------------------------------------===
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+# Copyright 2022 Battelle Memorial Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 #
-# This material was prepared as an account of work sponsored by an agency of
-# the United States Government. Neither the United States Government nor the
-# United States Department of Energy, nor Battelle, nor any of their
-# employees, nor any jurisdiction or organization that has cooperated in the
-# development of these materials, makes any warranty, express or
-# implied, or assumes any legal liability or responsibility for the accuracy,
-# completeness, or usefulness or any information, apparatus, product,
-# software, or process disclosed, or represents that its use would not infringe
-# privately owned rights. Reference herein to any specific commercial product,
-# process, or service by trade name, trademark, manufacturer, or otherwise
-# does not necessarily constitute or imply its endorsement, recommendation, or
-# favoring by the United States Government or any agency thereof, or
-# Battelle Memorial Institute. The views and opinions of authors expressed
-# herein do not necessarily state or reflect those of the
-# United States Government or any agency thereof.
-#
-# PACIFIC NORTHWEST NATIONAL LABORATORY operated by
-# BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
-# under Contract DE-AC05-76RL01830
+# ===----------------------------------------------------------------------===
 # }}}
 
 
 import gevent
 import pytest
 
-from volttron.utils import get_ops, get_examples, jsonapi
+from volttron.utils import jsonapi
 from volttron.client.known_identities import PLATFORM_TOPIC_WATCHER
 from volttron.utils.time import get_aware_utc_now
 
@@ -65,7 +50,8 @@ def test_remote_alert_publish(get_volttron_instances):
 
     def onmessage(peer, sender, bus, topic, headers, message):
         global alert_messages
-
+        print(f"Message received on topic: {topic}")
+        print(f"Message content: {message}")
         alert = jsonapi.loads(message)["context"]
 
         try:
@@ -98,13 +84,18 @@ def test_remote_alert_publish(get_volttron_instances):
     }
 
     alert_uuid = volttron_instance2.install_agent(
-        agent_dir=get_ops("TopicWatcher"),
+        agent_dir="volttron-topic-watcher",
         config_file=config,
         vip_identity=PLATFORM_TOPIC_WATCHER
     )
+    print("Subscribed to topic 'alerts'")
+    print("Publishing configuration:")
+    print(jsonapi.dumps(config, indent=2))
+    print(f"Installed agent with UUID: {alert_uuid}")
 
     gevent.sleep(6)
 
+    print("Alert messages received:", alert_messages)
     assert alert_messages
     alert_messages.clear()
 
@@ -163,7 +154,7 @@ def test_alert_multi_messagebus_publish(volttron_multi_messagebus):
     }
 
     alert_uuid = source_instance.install_agent(
-        agent_dir=get_ops("TopicWatcher"),
+        agent_dir="volttron-topic-watcher",
         config_file=config,
         vip_identity=PLATFORM_TOPIC_WATCHER
     )
